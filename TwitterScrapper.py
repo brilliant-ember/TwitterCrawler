045cc245d4bf6b4938dcab807fb2d4f51fd91e1a
brilliant-ember <fire.senfony@gmail.com>
2018-08-09 11:55:16 -0500

    filled some fields automatically

--- TwitterScrapper.py
+++ TwitterScrapper.py
@@ -1,26 +1,54 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-def keyWordSeach(*args):
+'''fills the "all of htses words" field in twitter advanced search
+args can be any number of strings, uses an & operator so all of these must be present
+'''
+def keyWordSearch(*args):
 	keyWords = driver.find_element_by_name('''ands''')
 	for word in args:
-		keyWords.send_keys(word)
+		keyWords.send_keys(" \' "+str(word)+ " \' ")
 		keyWords.send_keys(" ")
 
+'''Must be a single word'''
 def exactPhraseSearch(phrase):
 	exactPhrase = driver.find_element_by_name('''phrase''')
-	exactPhrase.send_keys(phrase)
+	exactPhrase.send_keys(str(phrase))
+
+def keyWordORSearch(*args):
+	keyWords = driver.find_element_by_name('''ors''')
+	for word in args:
+		keyWords.send_keys(" \' "+str(word)+ " \' ")
+		keyWords.send_keys(" ")
+
+def excludeSearch(*args):
+	keyWords = driver.find_element_by_name('''nots''')
+	for word in args:
+		keyWords.send_keys(" \' "+str(word)+ " \' ")
+		keyWords.send_keys(" ")
+def hastagSearch(*args):
+	keyWords = driver.find_element_by_name('''tag''')
+	for word in args:
+		keyWords.send_keys(" \' "+str(word)+ " \' ")
+		keyWords.send_keys(" ")
+
+
 
 driver = webdriver.Chrome("D:\\Programs\\chromedriver.exe")
 driver.get("https://twitter.com/search-advanced?lang=en&lang=en")
 #assert ("instagram" in driver.title), "are u sure this is Instagram page?"
-keyWordSeach("Rideau", "police")
-exactPhraseSearch("stab")
+keyWordSearch("Rideau mall")
+exactPhraseSearch("lol")
+keyWordORSearch("mike")
+excludeSearch("mike")
+hastagSearch("ok")
+
+
+
+#exactPhraseSearch(" ")
 
 
 #logIn.send_keys(Keys.RETURN)
 
 #driver.close()
 
-'''fills the "all of htses words" field in twitter advanced search
-args can be any number of strings'''
