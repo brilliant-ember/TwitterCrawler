9ac6d64d28dbde15f3dbd11e9fc5dfbb4e4d3344
brilliant-ember <fire.senfony@gmail.com>
2018-08-09 15:55:37 -0500

    more stuff

--- TwitterScrapper.py
+++ TwitterScrapper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 '''fills the "all of htses words" field in twitter advanced search
 args can be any number of strings, uses an & operator so all of these must be present
@@ -7,7 +8,7 @@ args can be any number of strings, uses an & operator so all of these must be pr
 def keyWordSearch(*args):
 	keyWords = driver.find_element_by_name('''ands''')
 	for word in args:
-		keyWords.send_keys(" \' "+str(word)+ " \' ")
+		keyWords.send_keys(str(word))
 		keyWords.send_keys(" ")
 
 '''Must be a single word'''
@@ -18,7 +19,7 @@ def exactPhraseSearch(phrase):
 def keyWordORSearch(*args):
 	keyWords = driver.find_element_by_name('''ors''')
 	for word in args:
-		keyWords.send_keys(" \' "+str(word)+ " \' ")
+		keyWords.send_keys(str(word))
 		keyWords.send_keys(" ")
 
 def excludeSearch(*args):
@@ -32,16 +33,40 @@ def hastagSearch(*args):
 		keyWords.send_keys(" \' "+str(word)+ " \' ")
 		keyWords.send_keys(" ")
 
+def locationSearch(place):
+	keyWords = driver.find_element_by_name('''near''')
+	keyWords.send_keys(str(place))
+
+
+
 
 
 driver = webdriver.Chrome("D:\\Programs\\chromedriver.exe")
 driver.get("https://twitter.com/search-advanced?lang=en&lang=en")
 #assert ("instagram" in driver.title), "are u sure this is Instagram page?"
-keyWordSearch("Rideau mall")
-exactPhraseSearch("lol")
-keyWordORSearch("mike")
-excludeSearch("mike")
-hastagSearch("ok")
+keyWordSearch("Rideau", "police")
+keyWordORSearch("stab", "stabbing")
+locationSearch("Rideau Street")
+driver.find_element_by_xpath('''//*[@id="page-container"]/div/div[1]/form/button''').click()
+#for the photos
+time.sleep(1)
+elems = driver.find_elements_by_tag_name('a')
+for e in range(len(elems)):
+	if (elems[e].text == "Photos"):
+		print("Found the photos nav thing")
+		elems[e].click()
+		break
+
+
+elems = driver.find_element_by_id("stream-items-id")
+elems = elems.find_elements_by_tag_name("span")
+for x in elems:
+	try:
+		x.find_element_by_tag_name("img").click()
+	except:
+		print("E")
+
+
 
 
 
@@ -49,6 +74,6 @@ hastagSearch("ok")
 
 
 #logIn.send_keys(Keys.RETURN)
-
+time.sleep(3)
 #driver.close()
 
