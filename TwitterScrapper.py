9fac586b449aef600e4ec1f134e07e3493035aa9
brilliant-ember <fire.senfony@gmail.com>
2018-08-10 11:33:30 -0500

    make the function return the list and limit the list to 25

--- TwitterScrapper.py
+++ TwitterScrapper.py
@@ -2,56 +2,92 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
+import urllib
+import sys
 
-def main():
-	def sleep(x):
-		time.sleep(x)
-	def keyWordSearch(*args):
-		'''fills the "all of htses words" field in twitter advanced search
-		args can be any number of strings, uses an & operator so all of these must be present
-		'''	
-		keyWords = driver.find_element_by_name('''ands''')
-		for word in args:
-			keyWords.send_keys(str(word))
-			keyWords.send_keys(" ")
+def stringDoubleQuoted(arg):
+	return '\"' + str(arg) + '\"'
 
-	'''Must be a single word'''
-	def exactPhraseSearch(phrase):
-		exactPhrase = driver.find_element_by_name('''phrase''')
-		exactPhrase.send_keys(str(phrase))
+def sleep(x):
+	time.sleep(x)
 
-	def keyWordORSearch(*args):
-		keyWords = driver.find_element_by_name('''ors''')
-		for word in args:
-			keyWords.send_keys(str(word))
-			keyWords.send_keys(" ")
+'''fills the "all of htses words" field in twitter advanced search
+args can be any number of strings, uses an & operator so all of these must be present
+'''
+def keyWordSearch(*args):
+	result = ''
+	for word in args:
+		result = result + str(word) + ' '
+	return result
 
-	def excludeSearch(*args):
-		keyWords = driver.find_element_by_name('''nots''')
-		for word in args:
-			keyWords.send_keys(" \' "+str(word)+ " \' ")
-			keyWords.send_keys(" ")
-	def hastagSearch(*args):
-		keyWords = driver.find_element_by_name('''tag''')
-		for word in args:
-			keyWords.send_keys(" \' "+str(word)+ " \' ")
-			keyWords.send_keys(" ")
+'''Must be a single word'''
+def exactPhraseSearch(phrase):
+	return stringDoubleQuoted(phrase) + ' '
 
-	def locationSearch(place):
-		keyWords = driver.find_element_by_name('''near''')
-		keyWords.send_keys(str(place))
+def keyWordORSearch(*args):
+	result = ''
+	if len(args) == 1: args = args[0].split()
+	count = 1
+	for word in args:
+		if(count > 1): result = result + 'OR '
+		result = result + str(word) + ' '
+		count = count + 1
+	return result
+
+def excludeSearch(*args):
+	result = ''
+	if len(args) == 1: args = args[0].split()
+	for word in args:
+		result = result + '-' + str(word) + ' '
+	return result
+
+def hastagSearch(*args):
+	result = ''
+	if len(args) == 1: args = args[0].split()
+	for word in args:
+		result = result + '#' + str(word) + ' '
+	return result
+
+def locationSearch(place):
+	return 'near:' + stringDoubleQuoted(place) + ' ' + 'within:15km' + ' '
+
+def sinceDate(fromDate):
+	return 'since:' + fromDate + ' '
+	
+def untilDate(toDate):
+	return 'until:' + toDate
+
+def clickSearch():
+	driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div/div[1]/form/button').click()
 
 
+def buildTestURL():
+	# Build the search url
+	url = ''
+	url = url + keyWordSearch("Rideau")
+	url = url + keyWordORSearch("stab", "stabbing","police")
+	#url = url + excludeSearch("lol")
+	#url = url + hastagSearch("ott")
+	url = url + locationSearch("Rideau Street")
+	url = url + sinceDate('2018-06-02')
+	#url = url + untilDate('2018-07-08')
 
+	url = url.strip()
+	url = urllib.parse.quote(url)
+	url = 'https://twitter.com/search?l=&q=' + url
+	url = url + '&src=typd'
+	return url
+	
+def doSearch(driverPath, url): 
+	# Chrome requires the complete path with the executable
+	driver = webdriver.Chrome(driverPath)
+	# Firefox requires only the directory path to the gecko driver
+	# "D:\soft\Python\python35-ws"
+	#driver = webdriver.Firefox(sys.argv[1])
+	#driver.get("https://twitter.com/search-advanced?lang=en")
 
+	driver.get(url)
 
-	driver = webdriver.Chrome("D:\\Programs\\chromedriver.exe")
-	driver.get("https://twitter.com/search-advanced?lang=en&lang=en")
-	#assert ("instagram" in driver.title), "are u sure this is Instagram page?"
-	keyWordSearch("Rideau")
-	keyWordORSearch("stab", "stabbing","police")
-	locationSearch("Rideau Street")
-	driver.find_element_by_xpath('''//*[@id="page-container"]/div/div[1]/form/button''').click()
 	#for the photos
 	sleep(2)
 	elems = driver.find_elements_by_tag_name('a')
@@ -61,11 +97,13 @@ def main():
 			elems[e].click()
 			break
 
-	sleep(3)
+	sleep(2)
 	elems = driver.find_element_by_id("stream-items-id")
 	elems = elems.find_elements_by_tag_name("span")
 	list_of_photos = []
-	for x in range(100):
+	for x in elems:
+		if len(list_of_photos) >= 25:
+			break
 		try:
 			x.click()
 			sleep(2)
@@ -77,15 +115,48 @@ def main():
 		except:
 			pass
 
-
-
-	for x in list_of_photos:
-		print("listItem ", end=x)
-	#exactPhraseSearch(" ")
-
-
-	#logIn.send_keys(Keys.RETURN)
-
 	driver.close()
+	return list_of_photos
 
-main()
+def main():
+	if len(sys.argv) != 2:
+		print ('''\
+		The path to the Gecko driver have to be passed
+		Usage: python TwitterScrapper.py D:\\Programs\\chromedriver.exe
+		''')
+		sys.exit(1)
+	url = buildTestURL()
+	doSearch(sys.argv[1], url)
+
+def performSeach(driver, keyWordSearchStr, keyWordORSearchStr, excludeSearchStr, hastagSearchStr, locationSearchStr, sinceDateStr, untilDateStr):
+	# Build the search url
+	url = ''
+	if keyWordSearchStr:
+		url = url + keyWordSearch(keyWordSearchStr)
+	if keyWordORSearchStr:
+		url = url + keyWordORSearch(keyWordORSearchStr)
+	if excludeSearchStr:
+		url = url + excludeSearch(excludeSearchStr)
+	if hastagSearchStr:
+		url = url + hastagSearch(hastagSearchStr)
+	if locationSearchStr:
+		url = url + locationSearch(locationSearchStr)
+	if sinceDateStr:
+		url = url + sinceDate(sinceDateStr)
+	if untilDateStr:
+		url = url + untilDate(untilDateStr)
+
+	print ("url:" + url)
+	url = url.strip()
+	url = urllib.parse.quote(url)
+	url = 'https://twitter.com/search?l=&q=' + url
+	url = url + '&src=typd'
+	print ("url:" + url)
+	return doSearch(driver, url)
+	
+if __name__ == '__main__':
+	print ('This program is being run by itself')
+	main()
+#else:
+	#print ('I am being imported from another module')
+	
