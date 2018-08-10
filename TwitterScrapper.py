8c835b739ce1370f4c13df8c73348f6ce86ad3d1
brilliant-ember <fire.senfony@gmail.com>
2018-08-10 09:18:11 -0500

    now we can see the embedded section

--- TwitterScrapper.py
+++ TwitterScrapper.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 import time
 
 '''fills the "all of htses words" field in twitter advanced search
 args can be any number of strings, uses an & operator so all of these must be present
 '''
+def sleep(x):
+	time.sleep(x)
 def keyWordSearch(*args):
 	keyWords = driver.find_element_by_name('''ands''')
 	for word in args:
@@ -44,12 +47,12 @@ def locationSearch(place):
 driver = webdriver.Chrome("D:\\Programs\\chromedriver.exe")
 driver.get("https://twitter.com/search-advanced?lang=en&lang=en")
 #assert ("instagram" in driver.title), "are u sure this is Instagram page?"
-keyWordSearch("Rideau", "police")
-keyWordORSearch("stab", "stabbing")
+keyWordSearch("Rideau")
+keyWordORSearch("stab", "stabbing","police")
 locationSearch("Rideau Street")
 driver.find_element_by_xpath('''//*[@id="page-container"]/div/div[1]/form/button''').click()
 #for the photos
-time.sleep(1)
+sleep(2)
 elems = driver.find_elements_by_tag_name('a')
 for e in range(len(elems)):
 	if (elems[e].text == "Photos"):
@@ -57,15 +60,21 @@ for e in range(len(elems)):
 		elems[e].click()
 		break
 
-
+sleep(2)
 elems = driver.find_element_by_id("stream-items-id")
 elems = elems.find_elements_by_tag_name("span")
 for x in elems:
 	try:
-		x.find_element_by_tag_name("img").click()
+		x.click()
+		sleep(2)
+		driver.find_element_by_xpath('''/html/body/div[5]/div[2]/div[4]/div/div[2]/div[4]/div[2]/div[4]/div/button/div/span[1]''').click()
+		sleep(2)
+		r = driver.find_element_by_xpath('''/html/body/div[5]/div[2]/div[4]/div/div[2]/div[4]/div[2]/div[4]/div/div/ul/li[2]/button''').click()
+		print(x.text)
+		x.Select()
+		driver.find_element_by_link_text('''Embed Tweet''').click()
 	except:
 		print("E")
-
 
 
 
