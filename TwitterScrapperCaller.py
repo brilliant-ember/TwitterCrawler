0102c7df091cd3e90314b4781f6de3ff563a6d57
basharaa <32464233+basharaa@users.noreply.github.com>
2018-08-10 11:50:42 -0400

    Added a import/caller to test for integration.

---
+++ TwitterScrapperCaller.py
@@ -0,0 +1,3 @@
+import TwitterScrapper
+
+TwitterScrapper.performSeach("D:\soft\Python\python35-ws\chromedriver.exe", "Rideau", "stab stabbing police", None, "", "Rideau Street", '2018-06-02', None)
